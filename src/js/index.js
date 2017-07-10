bf576952296c7912fba76c7140f7ba8086e43baa
Mikey Badr <mikey.badr@attck.com>
2017-07-10 12:17:20 -0400

    normalize circle size, style

--- src/js/index.js
+++ src/js/index.js
@@ -18,6 +18,7 @@ window.onload = function() {
 	renderYear()
 
 	function renderYear(){
+		var mostPopularNameNumber;
 		d3.json("static/names_parsed/yob" + currentDisplayYear + ".json", function(data) {
 			var filteredData = filterData(data);
 			drawYear(filteredData);
@@ -35,6 +36,7 @@ window.onload = function() {
 	//      })
 	//      .style("font-size", function(d){ return getRadius(d)})
 
+			mostPopularNameNumber = year[0].number;
 			var makeSvgs = d3.select(".d3target").selectAll("svg")
 				.data(year)
 				.enter()
@@ -52,7 +54,8 @@ window.onload = function() {
 				.attr("cy", function(d){ return getRadius(d)})
 				.attr("r", function(d){ return getRadius(d)})
 				.style("fill", function(d){
-					return d.sex == 'M' ? randomColor({hue:'blue', luminocity: 'dark'}) : randomColor({hue:'pink', luminocity:'dark'});
+					// return d.sex == 'M' ? randomColor({hue:'blue', luminocity: 'dark'}) : randomColor({hue:'pink', luminocity:'dark'});
+					return d.sex == 'M' ? '#AFf0f7' : '#FFC9EC';
 				});
 
 			var makeNameLabels = makeSvgs
@@ -60,24 +63,53 @@ window.onload = function() {
 				.text(function(d){return d.name})
 				.attr("font-family", "sans-serif")
 				.attr("text-anchor", "middle")
-				.attr("font-size", "10px")
+				.attr("font-size", function(d){
+					const min = 10;
+					if (getRadius(d)/3 < min){ return min }
+					return getRadius(d)/3;
+				})
 				.attr("x", function(d){ return getRadius(d)})
 				.attr("y", function(d){ return getRadius(d)})
+
+
+			var tooltip = makeCircles
+				.append('div')
+		    .style("position", "absolute")
+		    .style("z-index", "10")
+		    .style("visibility", "hidden")
+		    .text("a simple tooltip");
+
+		    makeCircles
+		    .on("mouseover", function(){return tooltip.style("visibility", "visible");})
+				.on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
+				.on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 
 			var makeNumberLabels = makeSvgs
 				.append("text")
 				.text(function(d){return d.number})
 				.attr("font-family", "sans-serif")
 				.attr("text-anchor", "middle")
-				.attr("font-size", "10px")
+				.attr("font-size", function(d){
+					const min = 10;
+					if (getRadius(d)/4 < min){ return min }
+					return getRadius(d)/4;
+				})
 				.attr("x", function(d){ return getRadius(d)})
 				.attr("x", function(d){ return getRadius(d)})
-				.attr("y", function(d){ return getRadius(d) + 10})
+				.attr("y", function(d){
+					if(getRadius(d) > 30){
+					 return getRadius(d)*1.25;
+					}
+				  return getRadius(d)*1.5;
+				})
 		}
 
 		function getRadius(nameObj){
-			var min = 20;
-			var radius = Math.sqrt((nameObj.number/Math.PI));
+			const relativePoplarity = nameObj.number/mostPopularNameNumber;
+			const mostPopularNameArea = 75000;
+			const min = 20;
+			const radius = Math.sqrt(((relativePoplarity * mostPopularNameArea)/Math.PI));
+			// var radius = 200 * relativePoplarity;
 
 			// if (radius > max){ return max }
 			if (radius < min){ return min }
@@ -85,18 +117,18 @@ window.onload = function() {
 		}
 	}
 
-	function filterData(data){
+	function filterData(data, limit){
 		var filteredData = data.filter(function(name){
 			if( currentMaxOccurances == Infinity){
 				return name.sex == currentDisplaySex &&
-						 parseInt(name.number) >= parseInt(currentMinOccurances);
+						 name.number >= currentMinOccurances;
 			} else{
 				return name.sex == currentDisplaySex &&
-						 parseInt(name.number) >= parseInt(currentMinOccurances) &&
-						 parseInt(name.number) <= parseInt(currentMaxOccurances);
+						 name.number >= currentMinOccurances &&
+						 name.number <= currentMaxOccurances;
 			}
 		});
-		filteredData = filteredData.slice(0, 1000);
+		// filteredData = filteredData.slice(0, limit || 1000);
 		return filteredData;
 	}
 
@@ -122,7 +154,8 @@ window.onload = function() {
 		});
 
 		$('#sexSelector').change(function(e){
-			currentDisplaySex = e.currentTarget.checked ? 'M' : 'F';
+			// currentDisplaySex = e.currentTarget.checked ? 'M' : 'F';
+			currentDisplaySex = e.currentTarget.value;
 			$('#currentSex').html(currentDisplaySex);
 			// renderYear();
 		});
@@ -144,7 +177,7 @@ window.onload = function() {
 			renderYear();
 		})
 
-		$('#minOccurancesInput').change(function(e){
+		$('#minOccurancesInput').blur(function(e){
 			// debugger;
 			if(e.currentTarget.value == ''){
 				currentMinOccurances = '0';
